fefcce5e5ed22c3f257eae21e7539a75f977742b
rkstarnerd <gemille.ford@gmail.com>
2015-03-05 23:34:10 -0500

    add route to video show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Myflix::Application.routes.draw do
+  root 'videos#index'
   get 'ui(/:action)', controller: 'ui'
   get '/home',        to: 'videos#index'
-  root 'videos#index'
+  
+
+  resources :videos, only: [:index, :show]
 end
