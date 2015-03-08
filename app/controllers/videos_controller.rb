d05e6ef997aa3749b9892179b321d8dd1227ee38
rkstarnerd <gemille.ford@gmail.com>
2015-03-08 00:45:31 -0500

    remove partials and iterate through categories

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,11 +1,10 @@
 class VideosController < ApplicationController
+  def index
+    @videos = Video.all
+    @categories = Category.all
+  end
 
-def index
-  @videos = Video.all
-end
-
-def show
-  @video = Video.find(params[:id])
-end
-
+  def show
+    @video = Video.find(params[:id])
+  end
 end
