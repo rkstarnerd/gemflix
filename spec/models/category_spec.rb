fcc635012f830392c5e6b94c81693a5ada37903e
rkstarnerd <gemille.ford@gmail.com>
2015-03-09 01:34:00 -0400

    use shoulda matchers in specs

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,18 +1,5 @@
 require 'spec_helper'
 
 describe Category do
-  it "saves itself" do
-    category = Category.new(name: "drama")
-    category.save
-    expect(Category.first).to eq(category)
-  end
-
-  it "has many videos through video_categories" do
-    category = Category.create(id: 1, name: "drama")
-    sherlock = Video.create(id: 1, title: "Sherlock", description: "sleuth")
-    break_bad = Video.create(id: 2,title: "Breaking Bad", description: "teacher who is a drug dealer")
-    video_category = VideoCategory.create(category_id: 1, video_id: 1)
-    video_category = VideoCategory.create(category_id: 1, video_id: 2)
-    expect(category.videos).to include(sherlock, break_bad)
-  end
+    it { should have_many(:videos) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,26 +1,9 @@
 require 'spec_helper'
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(title: "sherlock", description: "i am sherlocked")
-    video.save
-    expect(Video.first).to eq(video)
-  end
+  it { should have_many(:categories) }
 
-  it "has many categories through video_categories" do
-    sherlock = Video.create(id: 1, title: "sherlock", description: "sleuth")
-    drama    = Category.create(id: 1, name: "drama")
-    tv       = Category.create(id:2, name: "tv")
-    video_category = VideoCategory.create(category_id: 1, video_id: 1)
-    video_category = VideoCategory.create(category_id: 2, video_id: 1)
-    expect(sherlock.categories).to include(drama, tv)
-  end
+  it { should validate_presence_of(:title) }
 
-  it "requires a title" do
-    Video.new(title: "", description: "video description").should_not be_valid
-  end
-
-  it "requires a description" do
-    Video.new(title: "Video Title", description: "").should_not be_valid
-  end
+  it { should validate_presence_of(:description) }
 end
