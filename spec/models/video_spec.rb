fca9759939308a90f59e768fd8635d8e3dd2f43f
rkstarnerd <gemille.ford@gmail.com>
2015-03-09 00:02:01 -0400

    test video has many categories

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,4 +6,13 @@ describe Video do
     video.save
     expect(Video.first).to eq(video)
   end
+
+  it "has many categories through video_categories" do
+    sherlock = Video.create(id: 1, title: "sherlock")
+    drama    = Category.create(id: 1, name: "drama")
+    tv       = Category.create(id:2, name: "tv")
+    video_category = VideoCategory.create(category_id: 1, video_id: 1)
+    video_category = VideoCategory.create(category_id: 2, video_id: 1)
+    expect(sherlock.categories).to include(drama, tv)
+  end
 end
