235d828e3be1895e12a9f375cc54da52dc48028a
rkstarnerd <gemille.ford@gmail.com>
2015-03-09 00:32:49 -0400

    add description to videos

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -9,8 +9,8 @@ describe Category do
 
   it "has many videos through video_categories" do
     category = Category.create(id: 1, name: "drama")
-    sherlock = Video.create(id: 1, title: "Sherlock")
-    break_bad = Video.create(id: 2,title: "Breaking Bad")
+    sherlock = Video.create(id: 1, title: "Sherlock", description: "sleuth")
+    break_bad = Video.create(id: 2,title: "Breaking Bad", description: "teacher who is a drug dealer")
     video_category = VideoCategory.create(category_id: 1, video_id: 1)
     video_category = VideoCategory.create(category_id: 1, video_id: 2)
     expect(category.videos).to include(sherlock, break_bad)
