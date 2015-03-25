8c87d6bc325b67c7f5e065fca0331e7735b64b7e
rkstarnerd <gemille.ford@gmail.com>
2015-03-25 19:47:11 -0400

    define search action

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,10 +1,19 @@
 class VideosController < ApplicationController
+  before_action :set_categories, only: [:index, :search]
   def index
     @videos = Video.all
-    @categories = Category.all
   end
 
   def show
     @video = Video.find(params[:id])
+  end
+
+  def search
+    @videos = Video.search_by_title(params[:search_term])
+  end
+
+  private
+  def set_categories
+    @categories = Category.all
   end
 end
