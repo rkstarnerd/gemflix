0d813ec23fd88838fcb2bd3d0b6f186433af9144
rkstarnerd <gemille.ford@gmail.com>
2015-04-06 20:48:38 -0400

    def create and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to home_path
+    else
+      flash[:error] = "Your username and/or password was incorrect. Please try again."
+      redirect_to signin_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
 end
