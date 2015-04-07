8af55ad78443d98736e0e321a3539d521309f2bc
rkstarnerd <gemille.ford@gmail.com>
2015-04-06 20:22:54 -0400

    moved front page view to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Myflix::Application.routes.draw do
-  root 'videos#front'
+  root 'sessions#front'
   get 'ui(/:action)', controller: 'ui'
   get '/home',        to: 'videos#index'
   get '/signin',      to: 'sessions#new'
