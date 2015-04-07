359096b2c9f83ed467a17caa3ab1efd7d7b9f85a
rkstarnerd <gemille.ford@gmail.com>
2015-04-06 20:49:26 -0400

    def create action and user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to signin_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :email)
+  end
 end
+
