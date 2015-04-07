9eb391d6ba2281b813c22d5989db6d3fc2790b9b
rkstarnerd <gemille.ford@gmail.com>
2015-04-06 20:20:20 -0400

    require user except for front page, sign in, and registration pages

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,7 @@
 class VideosController < ApplicationController
   before_action :set_categories, only: [:index, :search]
+  before_action :require_user
+  
   def index
     @videos = Video.all
   end
