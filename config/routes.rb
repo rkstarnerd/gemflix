c64621aba66b18c08cb9d34785db4bff9130e66f
rkstarnerd <gemille.ford@gmail.com>
2015-04-07 22:21:58 -0400

    route register to users controller, new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Myflix::Application.routes.draw do
   root 'pages#front'
   get 'ui(/:action)', controller: 'ui'
+  get 'register',     to: "users#new"
   get '/home',        to: 'videos#index'
-  get '/signin',       to: 'sessions#new'
-  post '/signin',      to: 'sessions#create'
-  get '/signout',      to: 'sessions#destroy'
+  get '/signin',      to: 'sessions#new'
+  post '/signin',     to: 'sessions#create'
+  get '/signout',     to: 'sessions#destroy'
 
   resources :videos, only: [:index, :show] do
     collection do
