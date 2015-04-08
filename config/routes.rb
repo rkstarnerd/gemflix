8b6d18bcfea95aecd99a1d187099b75be61b75e0
rkstarnerd <gemille.ford@gmail.com>
2015-04-07 22:13:13 -0400

    change root to pages controller, front action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Myflix::Application.routes.draw do
-  root 'sessions#front'
+  root 'pages#front'
   get 'ui(/:action)', controller: 'ui'
   get '/home',        to: 'videos#index'
   get '/signin',       to: 'sessions#new'
