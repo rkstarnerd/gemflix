8516a2e1fe3b78ec05e170cf0fcb4116f484fac8
rkstarnerd <gemille.ford@gmail.com>
2015-04-13 20:56:14 -0400

    use let in spec to create videos instead of creating same videos for every test

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -7,34 +7,28 @@ describe Video do
 
   it { should validate_presence_of(:description) }
 
+  let(:sherlock) { Video.create(title: "Sherlock", description: "sleuth") }
+  let(:sherlock_bad) { Video.create(title: "Sherlock Bad", description: "sleuth", created_at: 1.day.ago) }
+  let(:breaking_bad) { Video.create(title: "Breaking Bad", description: "teacher turned drug dealer") }
+
   describe "search_by_title" do
     it "returns an empty array if there is no match" do
-      sherlock = Video.create(title: "Sherlock", description: "sleuth")
-      breaking_bad = Video.create(title: "Breaking Bad", description: "teacher turned drug dealer")
       expect(Video.search_by_title("hello")).to eq([])
     end
 
     it "returns an array of one video for an exact match" do
-      sherlock = Video.create(title: "Sherlock", description: "sleuth")
-      breaking_bad = Video.create(title: "Breaking Bad", description: "teacher turned drug dealer")
       expect(Video.search_by_title("Sherlock")).to eq([sherlock])
     end
 
     it "returns an array of one video for a partial match" do
-      sherlock = Video.create(title: "Sherlock", description: "sleuth")
-      breaking_bad = Video.create(title: "Breaking Bad", description: "teacher turned drug dealer")
       expect(Video.search_by_title("lock")).to eq([sherlock])
     end
 
     it "returns an array of all matches ordered by created_at" do
-      sherlock = Video.create(title: "Sherlock Bad", description: "sleuth", created_at: 1.day.ago)
-      breaking_bad = Video.create(title: "Breaking Bad", description: "teacher turned drug dealer")
-      expect(Video.search_by_title("bad")).to eq([breaking_bad, sherlock])
+      expect(Video.search_by_title("bad")).to eq([breaking_bad, sherlock_bad])
     end
 
     it "returns an empty array for a search with an empty string" do
-      sherlock = Video.create(title: "Sherlock Bad", description: "sleuth", created_at: 1.day.ago)
-      breaking_bad = Video.create(title: "Breaking Bad", description: "teacher turned drug dealer")
       expect(Video.search_by_title("")).to eq([])
     end
   end
