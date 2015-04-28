69fedbab72f189e65386e4f44560ab87890eb1a0
rkstarnerd <gemille.ford@gmail.com>
2015-04-28 18:57:25 -0400

    test that show action sets @video when user is logged in and redirects when user is not logged in

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -1,26 +1,37 @@
 require 'spec_helper'
 
 describe VideosController do
-
-  let(:sherlock) { Video.create(title: "Sherlock",   description: "sleuth") }
-  let(:matrix) { Video.create(title: "The Matrix", description: "the one") }
+  it { should use_before_action(:require_user)   }
 
   describe "GET show" do
-    it { should use_before_action(:require_user)   }
-
-    it "sets the @video variable" do
+    it "sets the @video variable when user is logged in" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(assigns(:video)).to eq(video)
     end
-    it "renders the show template" do
+  end
+
+    it "redirects to the sign in page when user is not logged in" do
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(response).to redirect_to signin_path
     end
   end
 
-  describe "GET search" do
-    it { should use_before_action(:set_categories) }
-    it { should use_before_action(:require_user)   }
+#   describe "GET search" do
+#     it { should use_before_action(:set_categories) }
 
-    it "sets the @videos variable" do
-    end
-    it "renders the search template" do
-    end
-  end
-end
+#     it "sets the @videos variable" do
+#     end
+#     it "renders the search template when user is logged in" do
+#       get :search
+#       expect(response).to render_template :search
+#     end
+
+#     it "does not render the search template when the user is not logged in" do
+#       get :search
+#       expect(response).to eq([signin_path])
+#     end
+#   end
+# end
