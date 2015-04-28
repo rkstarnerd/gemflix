0963d48a9880fea581aee305e7bca6c5665101ef
rkstarnerd <gemille.ford@gmail.com>
2015-04-28 19:12:38 -0400

    test that search sets @videos when logged in and redirects when not logged in

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -10,7 +10,6 @@ describe VideosController do
       get :show, id: video.id
       expect(assigns(:video)).to eq(video)
     end
-  end
 
     it "redirects to the sign in page when user is not logged in" do
       video = Fabricate(:video)
@@ -19,19 +18,19 @@ describe VideosController do
     end
   end
 
-#   describe "GET search" do
-#     it { should use_before_action(:set_categories) }
+  describe "POST search" do
+    it { should use_before_action(:set_categories) }
 
-#     it "sets the @videos variable" do
-#     end
-#     it "renders the search template when user is logged in" do
-#       get :search
-#       expect(response).to render_template :search
-#     end
+    it "sets the @videos variable when user is logged in" do
+      session[:user_id] = Fabricate(:user).id
+      sherlock = Fabricate(:video, title: 'Sherlock', description: 'sleuth')
+      post :search, search_term: 'lock'
+      expect(assigns(:videos)).to eq([sherlock])
+    end
 
-#     it "does not render the search template when the user is not logged in" do
-#       get :search
-#       expect(response).to eq([signin_path])
-#     end
-#   end
-# end
+    it "does not render the search template when the user is not logged in" do
+      get :search
+      expect(response).to redirect_to signin_path
+    end
+  end
+end
