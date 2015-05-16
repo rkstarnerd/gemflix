d3d2c67017ec7926c2c571fb2840a1b8413ce83f
rkstarnerd <gemille.ford@gmail.com>
2015-05-16 12:22:34 -0400

    add cover urls

--- spec/fabricators/video_fabricator.rb
+++ spec/fabricators/video_fabricator.rb
@@ -1,4 +1,6 @@
 Fabricator(:video) do
   title { Faker::Lorem.words(5).join(" ")}
   description { Faker::Lorem.paragraph(2) }
+  small_cover_url { Faker::Internet.url }
+  large_cover_url { Faker::Internet.url }
 end
