2e0af7ba9d41af6b43cd455205a342e4f468a62a
rkstarnerd <gemille.ford@gmail.com>
2015-05-16 12:21:13 -0400

    create average_rating method

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,13 @@ class Review < ActiveRecord::Base
 
   validates_presence_of :description
   validates_presence_of :rating
+
+  def self.average_rating(video)
+    ratings = video.reviews.map {|review| review.rating}
+    if video.reviews.count == 0
+      return "Be the first to review this video!"
+    else
+      (ratings.inject(:+))/(video.reviews.count)
+    end
+  end
 end
