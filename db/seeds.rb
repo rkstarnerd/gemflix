3afba0643c83904bfa1c229dcc85359605b9eb0f
rkstarnerd <gemille.ford@gmail.com>
2015-05-17 14:37:44 -0400

    create reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,6 @@ Video.create(title: "Sherlock", description: "A modern update finds the famous s
 Video.create(title: "The Matrix", description: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.", small_cover_url: "/tmp/the_matrix.jpg", large_cover_url: "/tmp/the_matrix_large.jpg")
 
 Video.create(title: "The Dark Knight", description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.", small_cover_url: "/tmp/dark_knight.jpg", large_cover_url: "/tmp/dark_knight_large.jpg")
+
+gemille  = User.create(name: "Gemille Ford", password: "password", email: "gemille@example.com")
+Review.create(user_id: gemille.id, video_id: 2, rating: 4, description: "This is an excellent whodunit. Instant classic. Looking forward to next season!")
