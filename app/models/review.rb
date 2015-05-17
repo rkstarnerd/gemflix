6f993cf5a9f606e8885e17092edba56d980eea23
rkstarnerd <gemille.ford@gmail.com>
2015-05-17 14:36:30 -0400

    make sum of ratings a float in average_rating method

--- app/models/review.rb
+++ app/models/review.rb
@@ -10,7 +10,7 @@ class Review < ActiveRecord::Base
     if video.reviews.count == 0
       return "Be the first to review this video!"
     else
-      (ratings.inject(:+))/(video.reviews.count)
+      (ratings.inject(:+).to_f)/(video.reviews.count)
     end
   end
 end
