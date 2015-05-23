210bab765fd0f882245263dd558a44a2f9b91e03
rkstarnerd <gemille.ford@gmail.com>
2015-05-23 17:37:06 -0400

    create review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,20 @@
-class Reviews < ApplicationController
+class ReviewsController < ApplicationController
+  before_action :require_user
+
   def new
     @review = Review.new
+  end
+
+  def create
+    @video  = Video.find(params[:video_id])
+    @reviews = @video.reviews.all.reload
+    review = @video.reviews.build(params.require(:review).permit(:description, :rating).merge!(user: current_user))
+
+    if review.save
+      redirect_to @video
+    else
+      flash[:error] = "Please include a review description."
+      render 'videos/show'
+    end
   end
 end
