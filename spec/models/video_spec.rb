f1a75c634bdce33aa426a89380ae9444614feb70
rkstarnerd <gemille.ford@gmail.com>
2015-05-23 17:48:04 -0400

    test relationship to reviews and order

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,6 +4,7 @@ describe Video do
   it { should have_many(:categories) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
+  it { should have_many(:reviews).order("created_at DESC")}
 
   let(:sherlock) { Video.create(title: "Sherlock", description: "sleuth") }
   let(:sherlock_bad) { Video.create(title: "Sherlock Bad", description: "sleuth", created_at: 1.day.ago) }
