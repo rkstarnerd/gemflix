b421ae5d7b509d73845bc133f2693379a36f8935
rkstarnerd <gemille.ford@gmail.com>
2015-05-25 19:15:51 -0400

    test create action

--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -16,4 +16,61 @@ describe QueueItemsController do
       expect(response).to redirect_to('/signin')
     end
   end
+
+  describe "POST create" do
+    it "redirects to the my queue page" do
+      session[:user_id] = Fabricate(:user)
+      video = Fabricate(:video)
+      post :create, video_id: video.id
+      expect(response).to redirect_to my_queue_path
+    end
+
+    it "creates queue_item" do
+      session[:user_id] = Fabricate(:user)
+      video = Fabricate(:video)
+      post :create, video_id: video.id
+      expect(QueueItem.count).to eq(1)
+    end
+
+    it "the queue item that is associated with the video" do
+      session[:user_id] = Fabricate(:user)
+      video = Fabricate(:video)
+      post :create, video_id: video.id
+      expect(QueueItem.first.video).to eq(video)
+    end
+
+    it "creates the queue item that is associated with the signed in user" do
+      user = Fabricate(:user)
+      session[:user_id] = user
+      video = Fabricate(:video)
+      post :create, video_id: video.id
+      expect(QueueItem.first.user).to eq(user)
+    end
+
+    it "puts the video as the last one in the queue" do
+      user = Fabricate(:user)
+      session[:user_id] = user
+      video = Fabricate(:video)
+      Fabricate(:queue_item, video: video, user: user)
+      video2 = Fabricate(:video)
+      post :create, video_id: video2.id
+      video2_queue_item = QueueItem.where(video_id: video2.id, user_id: user.id).first
+      expect(video2_queue_item.position).to eq(2)
+    end
+
+    it "does not add the video to the queue if it is already in the queue" do
+      user = Fabricate(:user)
+      session[:user_id] = user
+      video = Fabricate(:video)
+      Fabricate(:queue_item, video: video, user: user)
+      post :create, video_id: video.id
+      expect(user.queue_items.count).to eq(1)
+    end
+
+    it "redirects to the sign in page for unauthenticated users" do
+      video = Fabricate(:video)
+      post :create, video_id: video.id
+      expect(response).to redirect_to('/signin')
+    end
+  end
 end
