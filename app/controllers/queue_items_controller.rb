c95059b8c6ef9074c96bea456bf08da93d62bb94
rkstarnerd <gemille.ford@gmail.com>
2015-06-24 20:57:52 -0400

    add the normalize_queue_item_positions method to the destroy action

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -16,6 +16,7 @@ class QueueItemsController < ApplicationController
     user = current_user
     queue_item = QueueItem.find(params[:id])
     queue_item.destroy if current_user.queue_items.include?(queue_item)
+    normalize_queue_item_positions
     redirect_to my_queue_path
   end
 
