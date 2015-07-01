73fdce75d639ffab8e7f0c4df0d672466995b1f7
rkstarnerd <gemille.ford@gmail.com>
2015-06-30 21:32:07 -0400

    refactor using assign_category_to_videos macro

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -10,34 +10,25 @@ describe Category do
     it "returns the videos in the reverse order by created at" do  
       the_following = Video.create(id: 1, title: "The Following", description: "psychos", created_at: 1.day.ago)
       sherlock = Video.create(id: 2, title: "Sherlock", description: "detective")
-
-      Video.all.each do |video| 
-        VideoCategory.create(video_id: video.id, category_id: 1)
-      end
+      assign_category_to_videos    
       expect(drama.recent_videos).to eq([sherlock, the_following])
     end
 
     it "returns all of the videos if there are less than 6 videos" do
       2.times { create_video }    
-      Video.all.each do |video| 
-        VideoCategory.create(video_id: video.id, category_id: 1)
-      end
+      assign_category_to_videos
       expect(drama.recent_videos.count).to eq(2)
     end
 
     it "returns 6 videos if there are more than 6 videos" do
       7.times { create_video }
-      Video.all.each do |video| 
-        VideoCategory.create(video_id: video.id, category_id: 1)
-      end
+      assign_category_to_videos
       expect(drama.recent_videos.count).to eq(6)
     end
 
     it "returns the most recent 6 videos" do 
       6.times { create_video }
-      movies = Video.all.each do |video| 
-        VideoCategory.create(video_id: video.id, category_id: 1)
-      end
+      movies = assign_category_to_videos
       expect(drama.recent_videos).to eq(movies.reverse)
     end
 
