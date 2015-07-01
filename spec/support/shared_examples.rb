a0cf6c64d1c4719541ea1f38cd372721cc31b2a8
rkstarnerd <gemille.ford@gmail.com>
2015-06-30 20:57:32 -0400

    define 'redirects to my_queue_path'

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -5,3 +5,11 @@ shared_examples "requires sign in" do
     expect(response).to redirect_to signin_path
   end
 end
+
+shared_examples "redirects to my_queue_path" do
+  it "redirects to the my queue page" do
+      set_current_user
+      action
+      expect(response).to redirect_to my_queue_path
+    end
+end
