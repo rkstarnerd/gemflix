fc1fdc9f674259997d1e92bb4ccc9159802fdaa8
rkstarnerd <gemille.ford@gmail.com>
2015-07-02 22:23:54 -0400

    create user_interacts_with_queue feature spec

---
+++ spec/features/user_interacts_with_queue_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+feature "user interacts with the queue" do
+  scenario "user adds and reorders videos in the queue" do
+    drama = Category.create(id: 1, name: "drama")
+    video  = Fabricate(:video)
+    video2 = Fabricate(:video)
+    assign_category_to_videos
+    
+    user_signs_in
+    find("a[href='/videos/#{video.id}']").click
+    page.should have_content(video.title)
+    
+    click_link "+ My Queue"
+    page.should have_content(video.title)
+
+  end
+  
+end
