89c51a3e4156ff36a90e167fc7c85af6e7d83870
rkstarnerd <gemille.ford@gmail.com>
2015-08-04 19:56:01 -0400

    define relationship to following_relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :reviews,     -> { order(created_at: :desc) }
   has_many :queue_items, -> { order(:position) }
+  has_many :following_relationships, class_name: "Relationship", foreign_key: :follower_id
 
   validates_presence_of   :email, :password, :name
   validates_uniqueness_of :email
