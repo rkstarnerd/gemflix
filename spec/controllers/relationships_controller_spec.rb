d0c894b4b0471b0b504f6080a5a84c4e74421687
rkstarnerd <gemille.ford@gmail.com>
2015-08-04 20:00:43 -0400

    make alice current_user in test

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -4,11 +4,11 @@ describe RelationshipsController do
   describe "GET index" do
     it "sets @relationships to the current user following relationships" do
       alice = Fabricate(:user)
-      set_current_user
+      set_current_user(alice)
       bob = Fabricate(:user)
       relationship = Fabricate(:relationship, follower: alice, leader: bob)
       get :index
-      expect(assigns(:relationship)).to eq([relationship])
+      expect(assigns(:relationships)).to eq([relationship])
     end
   end
 end
