9ec620defd68ad326c8d12fd27d1b56e7d03218a
rkstarnerd <gemille.ford@gmail.com>
2015-08-04 21:44:33 -0400

    define create action, add create action to before_filter

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,5 @@
 class RelationshipsController < ApplicationController
-  before_filter :require_user, only: [:index, :destroy]
+  before_filter :require_user, only: [:index, :destroy, :create]
   def index
     @relationships = current_user.following_relationships
   end
@@ -7,6 +7,10 @@ class RelationshipsController < ApplicationController
   def destroy
     relationship = Relationship.find(params[:id])
     relationship.destroy if current_user == relationship.follower
+    redirect_to people_path
+  end
+
+  def create
     redirect_to people_path
   end
 end
