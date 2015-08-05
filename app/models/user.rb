b43a33382656b8ce92a755389c55cd8ac8047cf0
rkstarnerd <gemille.ford@gmail.com>
2015-08-04 20:27:30 -0400

    add relationship to leading_relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :reviews,     -> { order(created_at: :desc) }
   has_many :queue_items, -> { order(:position) }
   has_many :following_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :leading_relationships, class_name: "Relationship", foreign_key: :leader_id
 
   validates_presence_of   :email, :password, :name
   validates_uniqueness_of :email
