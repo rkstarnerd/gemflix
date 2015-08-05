edd708512b91da96fc07775324b988f851bf7476
rkstarnerd <gemille.ford@gmail.com>
2015-08-04 21:08:29 -0400

    test that delete action redirects to the people_path

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -21,8 +21,24 @@ describe RelationshipsController do
       let(:action) { delete :destroy, id: 4 }
     end
 
-    it "deletes the relationship if the current user is the follower" 
-    it "redirects to the people page"
+    it "redirects to the people page" do
+      alice = Fabricate(:user)
+      set_current_user(alice)
+      bob = Fabricate(:user)
+      relationship = Fabricate(:relationship, follower: alice, leader: bob)
+      delete :destroy, id: relationship
+      expect(response).to redirect_to people_path
+    end
+
+    it "deletes the relationship if the current user is the follower" do
+      alice = Fabricate(:user)
+      set_current_user(alice)
+      bob = Fabricate(:user)
+      relationship = Fabricate(:relationship, follower: alice, leader: bob)
+      delete :destroy, id: relationship
+      expect(Relationship.count).to eq(0)
+    end
+
     it "does not delete the relationship if the current user is not the follower"
   end
 end
