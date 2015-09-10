aa313a792628ef3033a71a870d03ca99c0a3a7ba
rkstarnerd <gemille.ford@gmail.com>
2015-09-10 19:11:38 -0400

    wrap env variables in quotes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,8 @@ Myflix::Application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            ENV_USERNAME,
-    password:             ENV_PASSWORD,
+    user_name:            'ENV_USERNAME',
+    password:             'ENV_PASSWORD',
     authentication:       'plain',
     enable_starttls_auto: true }
 end
