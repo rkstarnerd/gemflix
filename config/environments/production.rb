e849ac9c165271e40ab32aa800351c444fbddc95
rkstarnerd <gemille.ford@gmail.com>
2015-09-10 19:07:35 -0400

    add commas after each setting for actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,10 +21,10 @@ Myflix::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 587
-    domain:               'gmail.com'
-    user_name:            ENV_USERNAME
-    password:             ENV_PASSWORD
-    authentication:       'plain'
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV_USERNAME,
+    password:             ENV_PASSWORD,
+    authentication:       'plain',
     enable_starttls_auto: true }
 end
